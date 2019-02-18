2385e5b69f9bef6598db1fc2d3c7aaa933d07ab8
Johanna Jantzen <jjantzen@ufl.edu>
2019-02-18 11:26:13 -0500

    Script annotated

--- R_Scripts/Pruning_trees/Pruning_phylogram_random_subset_phylogenies.R
+++ R_Scripts/Pruning_trees/Pruning_phylogram_random_subset_phylogenies.R
@@ -7,7 +7,6 @@ subset300_list <- list.files("../Subset_analysis_files/300_taxon_subsets/Subsets
 subset400_list <- list.files("../Subset_analysis_files/400_taxon_subsets/Subsets/", pattern=".fasta", full.names=TRUE)
 subset500_list <- list.files("../Subset_analysis_files/500_taxon_subsets/Subsets/", pattern=".fasta", full.names=TRUE)
 
-
 #Write function to get names from fasta file
 get_names <- function(file){
   names <- read.fasta(file) %>% 
@@ -24,13 +23,14 @@ subset500_all_taxa <- lapply(subset500_list, get_names)
 
 #Read in 572_tree phylogram
 tree572 <- read.tree("./Basedata_Prep/Ordway_full_tree.tre", comment.char = "@")
-#Match tip labels
+#Match tip labels on tree to name format in subset lists
 tree572$tip.label <- gsub("'", "", tree572$tip.label)
 tree572$tip.label <- gsub("#\\d+_", "", tree572$tip.label)
 #Write tree with matched names
 write.tree(tree572, "./Basedata_Prep/Ordway_572_reduced_names.tre")
 
-#Prune phylogeny to each subset 
+#Prune phylogeny to each subset
+
 #Write function to prune phylogeny
 prune_tree <- function(taxa){
   prunedtree <- drop.tip(tree572,tree572$tip.label[-match(taxa, tree572$tip.label)])
@@ -43,14 +43,16 @@ all_300_trees <- lapply(subset300_all_taxa, prune_tree)
 all_400_trees <- lapply(subset400_all_taxa, prune_tree)
 all_500_trees <- lapply(subset500_all_taxa, prune_tree)
 
+#set class of trees
 class(all_100_trees) <- "multiPhylo"
 class(all_200_trees) <- "multiPhylo"
 class(all_300_trees) <- "multiPhylo"
 class(all_400_trees) <- "multiPhylo"
 class(all_500_trees) <- "multiPhylo"
 
-#Save each phylogeny for calculating PD in Biodiverse
-#Write function
+#Save each phylogeny for calculating PD
+
+#Write function to write trees
 write_subset_trees <- function(tree, filename){
   write.tree(tree, filename)
 }
@@ -71,7 +73,7 @@ names_of_400subsets <- gsub("../Subset_analysis_files/400_taxon_subsets/Subsets"
 names_of_500subsets <- gsub(".fasta", ".tre",subset500_list) 
 names_of_500subsets <- gsub("../Subset_analysis_files/500_taxon_subsets/Subsets", "./Subset_files/Pruned/Cladograms/Trees_500", names_of_500subsets)
 
-#Actually write trees
+#Actually write trees to folders
 mapply(write_subset_trees, tree=all_100_trees, filename=names_of_100subsets)
 mapply(write_subset_trees, tree=all_200_trees, filename=names_of_200subsets)
 mapply(write_subset_trees, tree=all_300_trees, filename=names_of_300subsets)
